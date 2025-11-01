d345acdccfb246295622eacaaf16cd86119f9377
Gianfranco Silvaledesma <gianfrancosilvaledesma@gmail.com>
2025-11-01 18:47:16 -0300

    intentando solucionar

--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./Contador.css"
+import "./contador.css"
 
 const Contador = ({ inicial, stock, onAdd }) => {
   const [cantidad, setCantidad] = useState(inicial);
